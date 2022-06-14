b1262e482677b2ec034b81f2756781c910a57aca
Sourabh-Bharale <89797467+Sourabh-Bharale@users.noreply.github.com>
2022-06-14 22:28:11 +0530

    code restructuring and bug fixes

--- Experience/Experience.js
+++ Experience/Experience.js
@@ -1,5 +1,5 @@
 import Sizes from "../Utils/Sizes"
-
+import Time from "../Utils/Time"
 export default class Experience{
     constructor(canvas){
         //global access
@@ -10,14 +10,24 @@ export default class Experience{
 
         //setup
         this.sizes=new Sizes()
+        this.time=new Time()
         
+        //sizes resize event
         this.sizes.on('resize',()=>{
                 this.resize()
+        })
+
+        //time tick event
+        this.time.on('tick',()=>{
+            this.update()
         })
     }
 
     resize(){
         console.log("resize occured");
         
+    }
+    update(){
+        console.log('experience Updated');
     }
 }
